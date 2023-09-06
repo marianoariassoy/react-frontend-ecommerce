9a943f651da5dd1f1683203c731e9745b5c948a0
marianoariassoy <hola@marianoarias.soy>
2023-09-06 12:07:51 -0300

    add url vercel

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <section className='text-center px-6 py-8 text-xs opacity-50 mt-12'>2023 COMISION 25555 CODERHOUSE BACKEND</section>
+    <section className='text-center px-6 py-8 text-xs opacity-50 mt-12'>2023 COMISION 55525 CODERHOUSE BACKEND</section>
   )
 }
 
